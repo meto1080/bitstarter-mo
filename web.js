5ace5a75a92cd78892c4f376d36f2ff8edb562a9
Ubuntu <ubuntu@ip-172-31-24-127.us-west-2.compute.internal>
2013-07-08 13:32:41 +0000

    Reading /etc/password??

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-fs.readFile('/', function (err, data) {
+fs.readFile('/index.html', function (err, data) {
                 if (err) throw err;
                 console.log(data);
 });
 
-app.get('/index.html', function(request, response) {
+app.get('/', function(request, response) {
   	response.send('Hello World3!');
 	console.log('test');
 });
